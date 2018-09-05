c4fb5c84a5327b13d61c34cd1f47315b7b9f5a53
nikopa <Kolyasik16062016>
2018-09-05 09:33:13 +0300

    AL01B Recursive Fibonacci Time: function body

---
+++ AL01B/src/Main.java
@@ -0,0 +1,8 @@
+import ee.ttu.algoritmid.fibonacci.AL01B;
+
+public class Main {
+    public static void main(String[] args) {
+        AL01B recursiveFibonacci = new AL01B();
+        System.out.println(recursiveFibonacci.recursiveF(4));
+    }
+}
