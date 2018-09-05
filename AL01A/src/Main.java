73bcfff61bcc159b2daa13ecd29ebdea4cb26f9e
nikopa <Kolyasik16062016>
2018-09-05 08:55:07 +0300

    AL01A Fibonnaci numbers. Fixed for tests

--- AL01A/src/Main.java
+++ AL01A/src/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         AL01A iterativeFibonacci = new AL01A();
+        System.out.println(iterativeFibonacci.iterativeF(0));
+        System.out.println(iterativeFibonacci.iterativeF(1));
+        System.out.println(iterativeFibonacci.iterativeF(2));
         System.out.println(iterativeFibonacci.iterativeF(3));
+        System.out.println(iterativeFibonacci.iterativeF(4));
+        System.out.println(iterativeFibonacci.iterativeF(5));
     }
 }
--- AL01A/src/ee/ttu/algoritmid/fibonacci/AL01A.java
+++ AL01A/src/ee/ttu/algoritmid/fibonacci/AL01A.java
@@ -1,5 +1,7 @@
 package ee.ttu.algoritmid.fibonacci;
 
+import java.math.BigInteger;
+
 public class AL01A {
 
     /**
@@ -8,12 +10,18 @@ public class AL01A {
      * @return The n-th number in Fibonacci series.
      */
     public String iterativeF(int n) {
-        int fib1 = 1;
-        int fib2 = 1;
-        int fib3;
+        BigInteger fib1;
+        if (n > 0) {
+            fib1 = new BigInteger("1");
+        } else {
+            fib1 = new BigInteger("0");
+        }
+
+        BigInteger fib2 = new BigInteger("1");
+        BigInteger fib3;
 
         for (int i = 2; i <= n; i++) {
-            fib3 = fib1 + fib2;
+            fib3 = fib1.add(fib2);
             fib1 = fib2;
             fib2 = fib3;
         }
