3bc1a59c8b73b61d2329520193a4769fc4c73897
nikopa96 <jertyk@gmail.com>
2018-09-05 19:35:54 +0300

    AL01B Recursive method time

--- AL01B/src/ee/ttu/algoritmid/fibonacci/AL01B.java
+++ AL01B/src/ee/ttu/algoritmid/fibonacci/AL01B.java
@@ -1,8 +1,12 @@
 package ee.ttu.algoritmid.fibonacci;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.RoundingMode;
 
 public class AL01B {
+
+    private static final String SECONDS_IN_YEAR = "31536000";
 
     /**
      * Estimate or find the exact time required to compute the n-th Fibonacci number.
@@ -10,7 +14,12 @@ public class AL01B {
      * @return The time estimate or exact time in YEARS.
      */
     public String timeToComputeRecursiveFibonacci(int n) {
-        return "";
+        BigInteger fibonacciNumber = recursiveF(n);
+
+        BigInteger rows = fibonacciNumber.multiply(new BigInteger("3")).subtract(new BigInteger("2"));
+        BigDecimal time2 = (new BigDecimal(rows)).divide(new BigDecimal(String.valueOf(n)), 6, RoundingMode.HALF_UP).divide(new BigDecimal(String.valueOf(SECONDS_IN_YEAR)), 6, RoundingMode.HALF_UP);
+
+        return String.valueOf(time2);
     }
 
     /**
