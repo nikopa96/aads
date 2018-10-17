74d7df754abce19e48dcda419b57301e8d111bc3
nikopa <nikopa@ttu.ee>
2018-10-17 20:11:06 +0300

    AL02_1 First versiond

--- AL04_1/src/Main.java
+++ AL04_1/src/Main.java
@@ -8,12 +8,20 @@ public class Main {
     public static void main(String[] args) {
         ScoreBoard scoreBoard = new ScoreBoard();
 
-        scoreBoard.add(new Participant(1, "a", 55));
-        scoreBoard.add(new Participant(2, "a", 55));
-        scoreBoard.add(new Participant(3, "a", 78));
-        scoreBoard.add(new Participant(4, "a", 94));
+//        scoreBoard.add(new Participant(1, "a", 55));
+//        scoreBoard.add(new Participant(2, "a", 55));
+//        scoreBoard.add(new Participant(3, "a", 78));
+//        scoreBoard.add(new Participant(4, "a", 94));
 
-        List<Participant> participants = scoreBoard.get(4);
+        scoreBoard.add(new Participant(5,"aa", 120));
+        scoreBoard.add(new Participant(2,"aa", 130));
+        scoreBoard.add(new Participant(3,"aa", 160));
+        scoreBoard.add(new Participant(4,"aa", 110));
+        scoreBoard.add(new Participant(1,"aa", 120));
+        scoreBoard.add(new Participant(1,"aa", 120));
+        scoreBoard.add(new Participant(2,"aa", 120));
+
+        List<Participant> participants = scoreBoard.get(7);
         participants.forEach(s -> System.out.println(s.getTime()));
     }
 }
