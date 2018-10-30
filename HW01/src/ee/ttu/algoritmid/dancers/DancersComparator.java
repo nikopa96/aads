d971ecc6d610452a9d4de4bb386767d3779bf9e7
nikopa <nikopa@ttu.ee>
2018-10-30 23:18:47 +0200

    HW01

--- HW01/src/ee/ttu/algoritmid/dancers/DancersComparator.java
+++ HW01/src/ee/ttu/algoritmid/dancers/DancersComparator.java
@@ -6,22 +6,13 @@ public class DancersComparator implements Comparator<Dancer> {
 
     @Override
     public int compare(Dancer dancer1, Dancer dancer2) {
-        //        int value1 = o1.campus.compareTo(o2.campus);
-//        if (value1 == 0) {
-//            int value2 = o1.faculty.compareTo(o2.faculty);
-//            if (value2 == 0) {
-//                return o1.building.compareTo(o2.building);
-//            } else {
-//                return value2;
-//            }
-//        }
-//        return value1;
-
         if (dancer1.getHeight() == dancer2.getHeight()) {
-            if (dancer1.getGender().equals(Dancer.Gender.FEMALE)) {
+            if (dancer1.getGender().equals(Dancer.Gender.FEMALE) && dancer1.getGender().equals(Dancer.Gender.MALE)) {
                 return -1;
-            } else {
+            } else if (dancer1.getGender().equals(Dancer.Gender.MALE) && dancer1.getGender().equals(Dancer.Gender.FEMALE)) {
                 return 1;
+            } else {
+                return 0;
             }
         } else if (dancer1.getHeight() < dancer2.getHeight()) {
             return -1;
