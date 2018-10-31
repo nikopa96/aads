f9476460391bae5b72c956037aa950c784b09878
nikopa <nikopa@ttu.ee>
2018-10-31 20:29:44 +0200

    AL05

--- AL05/src/ee/ttu/algoritmid/tsp/GreedyTSP.java
+++ AL05/src/ee/ttu/algoritmid/tsp/GreedyTSP.java
@@ -20,28 +20,37 @@ public class GreedyTSP {
 
     /* Greedy search */
     public static int[] greedySolution(int[][] adjacencyMatrix) {
-        int[] visitedTowns = new int[adjacencyMatrix.length + 1];
 
-        int j = 0;
-        for (int i = 0; i < visitedTowns.length; i++) {
-            adjacencyMatrix[j][j] = Integer.MAX_VALUE;
-            int min = findNearTown(adjacencyMatrix[j]);
+        if (adjacencyMatrix.length == 1) {
+            return new int[]{0};
+        } else {
+            int[] visitedTowns = new int[adjacencyMatrix.length + 1];
 
-            visitedTowns[i] = j;
-            adjacencyMatrix[min][j] = Integer.MAX_VALUE;
-            j = min;
+            int j = 0;
+            for (int i = 0; i < visitedTowns.length; i++) {
+                adjacencyMatrix[j][j] = Integer.MAX_VALUE;
+                int min = findNearTown(adjacencyMatrix[j]);
+
+                visitedTowns[i] = j;
+                adjacencyMatrix[min][j] = Integer.MAX_VALUE;
+                j = min;
+            }
+
+            visitedTowns[adjacencyMatrix.length] = 0;
+
+            return visitedTowns;
         }
-
-        visitedTowns[adjacencyMatrix.length] = 0;
-
-        return visitedTowns;
     }
 
     public static void main(String[] args) {
+//        int[][] adjacencyMatrix = {
+//                {0, 1, 5},
+//                {2, 0, 3},
+//                {3, 8, 0}
+//        };
+
         int[][] adjacencyMatrix = {
-                {0, 1, 5},
-                {2, 0, 3},
-                {3, 8, 0}
+                {0}
         };
 
         System.out.println(Arrays.toString(greedySolution(adjacencyMatrix)));
